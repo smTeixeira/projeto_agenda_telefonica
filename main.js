430d306cea5f562b63f4733e8c31fadd4c1c565d
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-02-20 16:32:17 -0300

    inicial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('form-agenda');
+const contato = [];
+const telefone = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+
+    contato.push(inputNomeContato.value);
+    telefone.push(inputTelefoneContato.value);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelefoneContato.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
